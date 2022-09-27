21b153fe298f9c48873477bf140a9756192e15d4
Tarun S Hegde <101446457+targithu@users.noreply.github.com>
2022-09-27 09:24:17 +0530

    two methods used

--- binarytrees/balanced_tree.cpp
+++ binarytrees/balanced_tree.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<pair>
 using namespace std;
 class node{
 public:
@@ -9,7 +10,7 @@ node(int d){
     this->data=d;
     this->left=NULL;
     this->right=NULL;
-}
+}};
 node* buildTree(node* root){
 cout<<"Enter the data: "<<endl;
 int data;
@@ -24,6 +25,7 @@ cout<<"Enter data for inserting in right"<<endl;
 root->right=buildTree(root->right);
 return root;
 }
+//first method-T.C:O(n**2)
 int height(node* root){
     if(root==NULL){
         return 0;
@@ -33,6 +35,7 @@ int height(node* root){
     int ans=max(left,right)+1;
     return ans;
 }
+//Function to check whether binary tree balanced or not.
  bool balanced(node* root){
         if(root==NULL) return true;
         bool left=balanced(root->left);
@@ -41,10 +44,30 @@ bool diff=abs(height(root->left)-height(root->right))<=1;
 if(left && right && diff) return 1 ;
 else return false;
 }
-
+//second method T.C-O(n)
+pair <bool,int> isBalancedFast(node* root){
+    if(root==NULL) {
+        pair<bool,int> p=make_pair(true,0);
+        return p;
+        }
+        pair<bool,int>left=isBalancedFast(root->left);
+        pair<bool,int>right=isBalancedFast(root->right);
+        bool leftAns=left.first;
+        bool rightAns=right.first;
+bool diff=abs(left.second-right.second)<=1;
+pair<bool,int> ans;
+ans.second=max(left.second,right.second)+1;
+if(leftAns && rightAns && diff) ans.first=1;
+else ans.first=false;
+return ans;
 }
+ bool balanced(node* root){
+return isBalancedFast(root).first;
+}
+
 int main()
 {    node* root=NULL;
     root=buildTree(root);
+    isBalancedFast(root);
     return 0;
 }
