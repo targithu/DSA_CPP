8c7aa560535c16812944e70a7c98895509eba523
Tarun S Hegde <101446457+targithu@users.noreply.github.com>
2022-09-27 17:51:26 +0530

    Add files via upload

---
+++ binarytrees/boundary_traversal.cpp
@@ -0,0 +1,57 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+class node{
+public:
+  int data;
+  node* left;
+  node* right;
+node(int d){
+    this->data=d;
+    this->left=NULL;
+    this->right=NULL;
+}
+};
+void traverseLeft(node* root,vector<int>&ans){
+if(root==NULL||(root->left==NULL && root->right==NULL)) return;
+ans.push_back(root->data);
+if(root->left) traverseLeft(root->left);
+else traverseLeft(root->right);
+}
+void traverseLeaf(node* root,vector<int>&ans){
+    //base case
+   if(root==NULL) return;
+   if(root->left==NULL && root->right==NULL){
+    ans.push_back(root->data);
+    return;
+   }
+   traverseLeaf(root->left,ans);
+   traverseLeaf(root->right,ans);
+   ans.push_back(root->data);
+}
+void traverseRight(node* root,vector<int>&ans){
+if(root==NULL||(root->left==NULL && root->right==NULL)) return;
+if(root->right) traverseRight(root->right,ans);
+else traverseRight(root->left,ans);
+ans.push_back(root->data);
+}
+vector<int> boundary(node* root){
+vector<int> ans;
+if(root==NULL) return ans;
+ans.push_back(root->data);
+//left part
+traverseLeft(root->left,ans);
+//traverse leaf nodes
+//left subtree
+traverseLeaf(root->left,ans);
+//right subtree
+traverseLeaf(root->right,ans);
+//right part
+traverseRight(root->right,ans);
+return ans;
+}
+
+int main()
+{
+    return 0;
+}
