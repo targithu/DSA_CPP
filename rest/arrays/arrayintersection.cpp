8bb58ab1238167ff3e84c39d6751ea917f029c79
Tarun S Hegde <101446457+targithu@users.noreply.github.com>
2022-09-29 21:05:17 +0530

    optimised

--- rest/arrays/arrayintersection.cpp
+++ rest/arrays/arrayintersection.cpp
@@ -2,19 +2,21 @@
 using namespace std;
 
 void inter(int arr1[],int n,int arr2[],int m){
-  for(int i=0;i<n;i++){
-   for(int j=0;j<m;j++){
-      if(arr1[i]==arr2[j]){
-          cout<<arr1[i]<<" ";
-      }
-  }
-
-  }cout<<endl;
+    int i=0,j=0;
+while(j!=m){
+    if(arr1[i]==arr2[j]){
+      cout<<arr1[i];
+      break;
+  }else{ 
+      i+=1;
+      if(i==n){
+        i=0;
+        j+=1;
+    }}}cout<<endl;
 }
 int main(){
  int arr1[8]={0,2,11,5,6,1,4,7};
  int arr2[3]={11,1,87};
  inter(arr1,8,arr2,3) ;
  return 0;
-
 }
