f955f02baf151ce66888aa9419ee52fcfa6388d1
Tarun S Hegde <101446457+targithu@users.noreply.github.com>
2022-09-29 21:13:32 +0530

    Update rotate_array.cpp

--- rest/arrays/rotate_array.cpp
+++ rest/arrays/rotate_array.cpp
@@ -1,11 +1,19 @@
 class Solution {
 public:
     void rotate(vector<int>& nums, int k) {
+        //soln 1
         vector<int>temp(nums.size());
       for(int i=0;i<nums.size();i++){
           int l=(i+k)%nums.size();
           temp[l]=nums[i];
       }
         nums=temp;
+        
+        
+        //soln 2
+         k = k % nums.size();
+        reverse(nums.begin(), nums.end());
+        reverse(nums.begin(), nums.begin() + k);
+        reverse(nums.begin() + k, nums.end());
     }
 };
